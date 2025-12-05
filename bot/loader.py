84e54557c041fc0d192afe60ca5af628df36b90d
Maksym Zavalniuk <mezgoodle@gmail.com>
2025-12-05 19:27:41 +0200

    Update aiogram bot initialization for version updates

--- bot/loader.py
+++ bot/loader.py
@@ -1,7 +1,11 @@
 from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
 from aiogram.fsm.storage.memory import MemoryStorage
 from tgbot.config import config
 
 storage = MemoryStorage()
-bot = Bot(token=config.bot_token.get_secret_value(), parse_mode="HTML")
+bot = Bot(
+    token=config.bot_token.get_secret_value(),
+    default=DefaultBotProperties(parse_mode="HTML"),
+)
 dp = Dispatcher(storage=storage)
