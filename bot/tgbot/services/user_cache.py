6a6039daf6f8d704dc8a096f7be7dec3e27fe071
Maksym Zavalniuk <mezgoodle@gmail.com>
2026-01-10 22:31:08 +0200

    Update user_cache service to the new response model

--- bot/tgbot/services/user_cache.py
+++ bot/tgbot/services/user_cache.py
@@ -1,18 +1,22 @@
-from typing import Iterable, Set
+from typing import Iterable
 
 
 class UserCache:
     def __init__(self):
-        self._allowed_ids: Set[int] = set()
+        self._allowed_ids: dict[int, int] = {}
 
-    def load(self, user_ids: Iterable[int]):
-        self._allowed_ids.update(user_ids)
+    def load(self, user_ids: Iterable[dict]) -> None:
+        for u in user_ids:
+            self._allowed_ids[u["telegram_id"]] = u["id"]
 
-    def add(self, user_id: int):
-        self._allowed_ids.add(user_id)
+    def add(self, user_id: int, telegram_id: int) -> None:
+        self._allowed_ids[telegram_id] = user_id
 
-    def is_allowed(self, user_id: int) -> bool:
-        return user_id in self._allowed_ids
+    def is_allowed(self, telegram_id: int) -> bool:
+        return telegram_id in self._allowed_ids
+
+    def get_user_id(self, telegram_id: int) -> int | None:
+        return self._allowed_ids.get(telegram_id)
 
     def __repr__(self):
         return f"<UserCache count={len(self._allowed_ids)}>"
