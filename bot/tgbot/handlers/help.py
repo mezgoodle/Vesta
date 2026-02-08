ad7a5cd0974daffd1ea88754ce17445c9b2a7923
Maksym Zavalniuk <mezgoodle@gmail.com>
2026-02-08 16:48:30 +0200

    Update help command

--- bot/tgbot/handlers/help.py
+++ bot/tgbot/handlers/help.py
@@ -1,11 +1,28 @@
-from aiogram.types import Message
 from aiogram.dispatcher.filters import CommandHelp
-
+from aiogram.types import Message
+from aiogram.utils.markdown import hbold
 from loader import dp
-from tgbot.keyboards.inline.help_keyboard import create_markup
 
 
 @dp.message_handler(CommandHelp(), state="*")
 async def help_command(message: Message) -> Message:
-    markup = create_markup()
-    await message.reply(f"Hello, {message.from_user.username}!", reply_markup=markup)
+    text = f"""
+    Hello, {hbold(message.from_user.username)}!
+    If you are new here, you can start by typing "/start" or "/help".
+    Also you need to be approved by admin({hbold("@sylvenis")}) to use this bot.
+
+    {hbold("Conversation with AI:")}
+    - To start conversation, type "/new"
+    - To get list of conversations, type "/chats"
+    - To reset the current conversation, type "/reset"
+
+    {hbold("Weather:")}
+    - To get weather in a city, type "/weather <city>"
+
+    {hbold("News:")}
+    - To get news, type "/news <category>"
+
+    {hbold("Calendar:")}
+    - To get calendar, type "/calendar"
+    """
+    await message.reply(text)
