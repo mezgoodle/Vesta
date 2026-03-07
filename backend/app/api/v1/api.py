b6bcf39ec6e58120187426c7e01a198be3e618b2
Maksym Zavalniuk <mezgoodle@gmail.com>
2026-03-07 20:17:38 +0200

    Add new sessions endpoint

--- backend/app/api/v1/api.py
+++ backend/app/api/v1/api.py
@@ -7,6 +7,7 @@ from app.api.v1.endpoints import (
     google_auth,
     login,
     news,
+    sessions,
     users,
     weather,
 )
@@ -15,6 +16,7 @@ api_router = APIRouter()
 api_router.include_router(login.router, prefix="/login", tags=["login"])
 api_router.include_router(users.router, prefix="/users", tags=["users"])
 api_router.include_router(chat.router, prefix="/chat", tags=["chat"])
+api_router.include_router(sessions.router, prefix="/sessions", tags=["sessions"])
 api_router.include_router(devices.router, prefix="/devices", tags=["devices"])
 api_router.include_router(news.router, prefix="/news", tags=["news"])
 api_router.include_router(weather.router, prefix="/weather", tags=["weather"])
